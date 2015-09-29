e21e86dbbcb4414a360ec5d7977f93680d8e40cb
Thomas Elvin <sde.thom855j@gmail.com>
2015-09-30 01:44:35 +0200

    Update Url.php

--- src/Url.php
+++ src/Url.php
@@ -5,21 +5,10 @@ namespace WebSupportDK\PHPHttp;
 class Url
 {
 
-    public static
-            function redirect($location = null)
+    public static function redirect($location = null)
     {
         if ($location)
         {
-            if (is_numeric($location))
-            {
-                switch ($location)
-                {
-                    case 404:
-                        header('HTTP/1.0 404 Not Found');
-                        exit();
-                        break;
-                }
-            }
             if (!headers_sent())
             {
                 header('Location: ' . $location);
@@ -38,64 +27,51 @@ class Url
         }
     }
 
-    public static
-            function redirectBack()
+    public static function redirectBack()
     {
         header('Location: javascript://history.go(-1)');
         exit;
     }
 
-    public static
-            function getRoot($path = '')
+    public static function getRoot($path = '')
     {
-        return self::getProtocol() . $_SERVER['HTTP_HOST'] . str_replace($path,
+        return self::getTransferProtocol() . self::getHost() . str_replace($path,
                                                                          '',
                                                                          dirname($_SERVER['SCRIPT_NAME']));
     }
 
-    public static
-            function get()
+    public static function get()
     {
-        return self::getProtocol() . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+        return self::getTransferProtocol() . self::getHost() . self::getUri();
     }
 
-    public static
-            function getError()
+    public static function getError()
     {
         return http_response_code();
     }
-
-    public static
-            function getHost()
+    
+    public static function setError($error,$phrase)
     {
-        return self::getProtocol() . $_SERVER['HTTP_HOST'];
+        return header(self::getServerProtocol() . " $error $phrase")
+    }
+    
+    public static function getUri()
+    {
+        return $_SERVER['REQUEST_URI'];
     }
 
-    public static
-            function getProtocol()
+    public static function getHost()
     {
-        /**
-         * Configuration for: URL
-         * Here we auto-detect your applications URL and the potential sub-folder. Works perfectly on most servers and in local
-         * development environments (like WAMP, MAMP, etc.). Don't touch this unless you know what you do.
-         *
-         * URL_PUBLIC_FOLDER:
-         * The folder that is visible to public, users will only have access to that folder so nobody can have a look into
-         * "/application" or other folder inside your application or call any other .php file than index.php inside "/public".
-         *
-         * URL_PROTOCOL:
-         * The protocol. Don't change unless you know exactly what you do.
-         *
-         * URL_DOMAIN:
-         * The domain. Don't change unless you know exactly what you do.
-         *
-         * URL_SUB_FOLDER:
-         * The sub-folder. Leave it like it is, even if you don't use a sub-folder (then this will be just "/").
-         *
-         * URL:
-         * The final, auto-detected URL (build via the segments above). If you don't want to use auto-detection,
-         * then replace this line with full URL (and sub-folder) and a trailing slash.
-         */
+        return $_SERVER['HTTP_HOST'];
+    }
+    
+    public static function getServerProtocol()
+    {
+        return $_SERVER['SERVER_PROTOCOL'];
+    }
+
+    public static function getTransferProtocol()
+    {
         $isSecure = false;
         if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on')
         {
