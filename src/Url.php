caf83bb4b0bcd063b539a347d056893a27be3655
Thomas Elvin <sde.thom855j@gmail.com>
2015-09-30 01:47:54 +0200

    Update Url.php

--- src/Url.php
+++ src/Url.php
@@ -1,89 +1,77 @@
-<?php
-
-namespace WebSupportDK\PHPHttp;
+<?php namespace WebSupportDK\PHPHttp;
 
 class Url
 {
 
-    public static function redirect($location = null)
-    {
-        if ($location)
-        {
-            if (!headers_sent())
-            {
-                header('Location: ' . $location);
-                exit;
-            }
-            else
-            {
-                echo '<script type="text/javascript">';
-                echo 'window.location.href="' . $location . '";';
-                echo '</script>';
-                echo '<noscript>';
-                echo '<meta http-equiv="refresh" content="0;url=' . $location . '" />';
-                echo '</noscript>';
-                exit;
-            }
-        }
-    }
+	public static function redirect($location = null)
+	{
+		if ($location) {
+			if (!headers_sent()) {
+				header('Location: ' . $location);
+				exit;
+			} else {
+				echo '<script type="text/javascript">';
+				echo 'window.location.href="' . $location . '";';
+				echo '</script>';
+				echo '<noscript>';
+				echo '<meta http-equiv="refresh" content="0;url=' . $location . '" />';
+				echo '</noscript>';
+				exit;
+			}
+		}
+	}
 
-    public static function redirectBack()
-    {
-        header('Location: javascript://history.go(-1)');
-        exit;
-    }
+	public static function redirectBack()
+	{
+		header('Location: javascript://history.go(-1)');
+		exit;
+	}
 
-    public static function getRoot($path = '')
-    {
-        return self::getTransferProtocol() . self::getHost() . str_replace($path,
-                                                                         '',
-                                                                         dirname($_SERVER['SCRIPT_NAME']));
-    }
+	public static function getRoot($path = '')
+	{
+		return self::getTransferProtocol() . self::getHost() . str_replace($path, '', dirname($_SERVER['SCRIPT_NAME']));
+	}
 
-    public static function get()
-    {
-        return self::getTransferProtocol() . self::getHost() . self::getUri();
-    }
+	public static function get()
+	{
+		return self::getTransferProtocol() . self::getHost() . self::getUri();
+	}
 
-    public static function getError()
-    {
-        return http_response_code();
-    }
-    
-    public static function setError($error,$phrase)
-    {
-        return header(self::getServerProtocol() . " $error $phrase")
-    }
-    
-    public static function getUri()
-    {
-        return $_SERVER['REQUEST_URI'];
-    }
+	public static function getError()
+	{
+		return http_response_code();
+	}
 
-    public static function getHost()
-    {
-        return $_SERVER['HTTP_HOST'];
-    }
-    
-    public static function getServerProtocol()
-    {
-        return $_SERVER['SERVER_PROTOCOL'];
-    }
+	public static function setError($error, $phrase)
+	{
+		return header(self::getServerProtocol() . " $error $phrase");
+	}
 
-    public static function getTransferProtocol()
-    {
-        $isSecure = false;
-        if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on')
-        {
-            $isSecure = true;
-        }
-        elseif (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] ==
-                'https' || !empty($_SERVER['HTTP_X_FORWARDED_SSL']) && $_SERVER['HTTP_X_FORWARDED_SSL'] ==
-                'on')
-        {
-            $isSecure = true;
-        }
-        return $isSecure ? 'https://' : 'http://';
-    }
+	public static function getUri()
+	{
+		return $_SERVER['REQUEST_URI'];
+	}
 
+	public static function getHost()
+	{
+		return $_SERVER['HTTP_HOST'];
+	}
+
+	public static function getServerProtocol()
+	{
+		return $_SERVER['SERVER_PROTOCOL'];
+	}
+
+	public static function getTransferProtocol()
+	{
+		$isSecure = false;
+		if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') {
+			$isSecure = true;
+		} elseif (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] ==
+			'https' || !empty($_SERVER['HTTP_X_FORWARDED_SSL']) && $_SERVER['HTTP_X_FORWARDED_SSL'] ==
+			'on') {
+			$isSecure = true;
+		}
+		return $isSecure ? 'https://' : 'http://';
+	}
 }
